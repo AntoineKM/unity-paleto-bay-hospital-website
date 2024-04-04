e53c90e52ba33041dec021945500833d07311dec
Antoine Kingue <contact@antoinek.fr>
2024-04-04 10:58:26 +0200

    💄 update theme config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
-import { KitchnProvider } from "kitchn";
+import { KitchnProvider, createTheme, darkTheme } from "kitchn";
 import { AppProps } from "next/app";
 
 import "kitchn/fonts.css";
+import { accentColors } from "../services/themes";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <KitchnProvider>
+    <KitchnProvider
+      themes={{
+        dark: createTheme({
+          ...darkTheme,
+          colors: {
+            ...darkTheme.colors,
+            accent: accentColors,
+          },
+        }),
+        light: createTheme({
+          ...darkTheme,
+          name: "light",
+          colors: {
+            ...darkTheme.colors,
+            accent: accentColors,
+          },
+        }),
+      }}
+    >
       <Component {...pageProps} />
     </KitchnProvider>
   );
---
+++ src/services/themes/index.ts
@@ -0,0 +1,12 @@
+import { AccentColors } from "kitchn";
+
+export const accentColors: AccentColors = {
+  primary: "rgb(17, 160, 188)",
+  secondary: "rgb(39, 168, 193)",
+  success: "rgb(46, 204, 113)",
+  warning: "rgb(241, 196, 15)",
+  danger: "rgb(231, 76, 60)",
+  info: "rgb(52, 152, 219)",
+  light: "rgb(255, 255, 255)",
+  dark: "rgb(0, 0, 0)",
+};
