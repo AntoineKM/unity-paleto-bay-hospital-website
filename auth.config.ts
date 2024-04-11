50f195c1bd0f99051dde916902ddebe24060b70d
Antoine Kingue <contact@antoinek.fr>
2024-04-11 11:30:42 +0200

    🔧 add auth config

---
+++ auth.config.ts
@@ -0,0 +1,57 @@
+import type { NextAuthConfig } from "next-auth";
+import Discord from "next-auth/providers/discord";
+
+import { env } from "@/env.mjs";
+
+// import { siteConfig } from "@/config/site"
+// import { getUserByEmail } from "@/lib/user";
+// import MagicLinkEmail from "@/emails/magic-link-email"
+// import { prisma } from "@/lib/db"
+
+const scopes = ["identify", "email"];
+
+export default {
+  providers: [
+    Discord({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+      authorization: { params: { scope: scopes.join(" ") } },
+    }),
+    // Email({
+    //   sendVerificationRequest: async ({ identifier, url, provider }) => {
+    //     const user = await getUserByEmail(identifier);
+    //     if (!user || !user.name) return null;
+
+    //     const userVerified = user?.emailVerified ? true : false;
+    //     const authSubject = userVerified ? `Sign-in link for ${siteConfig.name}` : "Activate your account";
+
+    //     try {
+    //       const { data, error } = await resend.emails.send({
+    //         from: 'SaaS Starter App <onboarding@resend.dev>',
+    //         to: process.env.NODE_ENV === "development" ? 'delivered@resend.dev' : identifier,
+    //         subject: authSubject,
+    //         react: MagicLinkEmail({
+    //           firstName: user?.name as string,
+    //           actionUrl: url,
+    //           mailType: userVerified ? "login" : "register",
+    //           siteName: siteConfig.name
+    //         }),
+    //         // Set this to prevent Gmail from threading emails.
+    //         // More info: https://resend.com/changelog/custom-email-headers
+    //         headers: {
+    //           'X-Entity-Ref-ID': new Date().getTime() + "",
+    //         },
+    //       });
+
+    //       if (error || !data) {
+    //         throw new Error(error?.message)
+    //       }
+
+    //       // console.log(data)
+    //     } catch (error) {
+    //       throw new Error("Failed to send verification email.")
+    //     }
+    //   },
+    // }),
+  ],
+} satisfies NextAuthConfig;
